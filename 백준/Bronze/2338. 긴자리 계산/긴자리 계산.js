3755fbfd2488f1551ce8ad4cc2492d3c98e66ac1
SeugnMin98 <158240718+SeungMin-Baek98@users.noreply.github.com>
2024-08-16 14:17:49 +0900

    [Bronze V] Title: 긴자리 계산, Time: 92 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/2338. 긴자리 계산/긴자리 계산.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const A = BigInt(input[0]);
+const B = BigInt(input[1]);
+
+console.log((A + B).toString());
+console.log((A - B).toString());
+console.log((A * B).toString());
