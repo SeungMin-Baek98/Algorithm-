613080d63f07a57b8325910ebb1807670923110e
SeugnMin98 <158240718+SeungMin-Baek98@users.noreply.github.com>
2024-08-21 15:54:45 +0900

    [Bronze V] Title: 사칙연산, Time: 96 ms, Memory: 9604 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "dev/stdin" : "input.txt";
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+const a = input[0];
+const b = input[1];
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(Math.floor(a / b));
+console.log(a % b);
