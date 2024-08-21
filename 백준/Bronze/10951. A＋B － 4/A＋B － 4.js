04ce5971bf4b3e47a0be983d9e5219fbfa447bd5
SeugnMin98 <158240718+SeungMin-Baek98@users.noreply.github.com>
2024-08-21 16:36:30 +0900

    [Bronze V] Title: A+B - 4, Time: 100 ms, Memory: 9564 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "dev/stdin" : "input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+for (let i = 0; i < input.length; i++) {
+  const a = input[i].split(" ");
+  console.log(Number(a[0]) + Number(a[1]));
+}
