afd89e128837f6b68211e26458c6df336f28c8f4
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2024-08-24 15:31:20 +0900

    [Bronze V] Title: 직사각형, Time: 88 ms, Memory: 9584 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "dev/stdin" : "input.txt";
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
+
+console.log(Number(input[0]) * Number(input[1]));
