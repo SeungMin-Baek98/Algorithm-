19817e19884b8ee09a8e893862b287a28aafbed7
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2024-08-24 16:13:41 +0900

    [Bronze V] Title: HI-ARC=?, Time: 92 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/28444. HI－ARC＝？/HI－ARC＝？.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "dev/stdin" : "input.txt";
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+console.log(input[0] * input[1] - input[2] * input[3] * input[4]);
