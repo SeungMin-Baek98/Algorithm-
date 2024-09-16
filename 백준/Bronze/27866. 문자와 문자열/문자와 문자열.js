9d92517aeb3557aa676b0da0603292453875cd16
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2024-09-16 17:38:01 +0900

    [Bronze V] Title: 문자와 문자열, Time: 92 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.js
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
-const filePath = process.platform === "linux" ? "dev/stdin" : "input.txt";
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-let a = input[0]; // 단어
-let b = input[1]; // 몇번쨰 인덱스에 무슨 알파벳이있는지,,
+const word = input[0];
+const findWord = input[1];
 
-console.log(a[b - 1]);
+console.log(word[findWord - 1]);
