79a0ea6894e035489f5019091185602c1091732f
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2024-10-17 14:56:34 +0900

    [Silver V] Title: 단어 정렬, Time: 216 ms, Memory: 20388 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const strs = Number(input[0]);
+let words = [];
+
+for (let i = 1; i <= strs; i++) {
+  words.push(input[i]);
+}
+
+// 중복된 값 제거를 함과 동시에 객체로 저장되는 값들을 배열로
+// 저장하기위하여 자바스크립트 spread 문법 사용
+
+words = [...new Set(words)];
+
+words.sort((a, b) => {
+  if (a.length === b.length) {
+    return a.localeCompare(b);
+  } else {
+    return a.length - b.length;
+  }
+});
+
+console.log(words.join("\n"));
