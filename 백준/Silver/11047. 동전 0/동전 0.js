c4c33d294d9e5aab7284a2e1f69767de48d2d4d6
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2024-11-02 15:35:23 +0900

    [Silver IV] Title: 동전 0, Time: 96 ms, Memory: 9596 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.js
+++ 백준/Silver/11047. 동전 0/동전 0.js
@@ -2,21 +2,22 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-let [count, money] = input[0].split(" ").map(Number);
+let [count, money] = input[0].split(" ").map(Number); // 동전 개수 및 가격
 const coins = input
   .slice(1)
   .map(Number)
   .sort((a, b) => b - a);
 
 let result = 0;
-
-for (const coin of coins) {
+for (let i = 0; i < coins.length; i++) {
+  const coin = coins[i];
   if (money === 0) break;
 
   if (coin <= money) {
     const coinNum = Math.floor(money / coin);
-    money -= coinNum * coin;
+    money -= coin * coinNum;
     result += coinNum;
   }
 }
+
 console.log(result);
