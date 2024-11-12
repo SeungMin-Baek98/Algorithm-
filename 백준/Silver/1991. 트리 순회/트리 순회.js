d3be635307a25d96176afc8694717b14ca4b30cf
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2024-11-12 13:43:17 +0900

    [Silver I] Title: 트리 순회, Time: 92 ms, Memory: 9352 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.js
+++ 백준/Silver/1991. 트리 순회/트리 순회.js
@@ -2,67 +2,45 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
+const N = Number(input.shift());
+let result = "";
+
+const tree = {};
+for (let i = 0; i < N; i++) {
+  const [node, left, right] = input[i].split(" ");
+  tree[node] = [left, right];
+}
+
+function preOrder(node) {
+  if (node === ".") return;
+  else {
+    const [left, right] = tree[node];
+    result += node;
+    preOrder(left);
+    preOrder(right);
   }
 }
 
-class BinaryTree {
-  constructor() {
-    this.nodes = {};
-  }
-
-  addNode(value, left, right) {
-    if (!this.nodes[value]) this.nodes[value] = new Node(value);
-    if (left !== ".") {
-      this.nodes[left] = new Node(left);
-      this.nodes[value].left = this.nodes[left];
-    }
-    if (right !== ".") {
-      this.nodes[right] = new Node(right);
-      this.nodes[value].right = this.nodes[right];
-    }
-  }
-
-  preOrder(node, result = []) {
-    if (node) {
-      result.push(node.value);
-      this.preOrder(node.left, result);
-      this.preOrder(node.right, result);
-    }
-    return result;
-  }
-
-  inOrder(node, result = []) {
-    if (node) {
-      this.inOrder(node.left, result);
-      result.push(node.value);
-      this.inOrder(node.right, result);
-    }
-    return result;
-  }
-
-  postOrder(node, result = []) {
-    if (node) {
-      this.postOrder(node.left, result);
-      this.postOrder(node.right, result);
-      result.push(node.value);
-    }
-    return result;
-  }
+function inOrder(node) {
+  if (node === ".") return;
+  const [left, right] = tree[node];
+  inOrder(left);
+  result += node;
+  inOrder(right);
 }
 
-const n = parseInt(input[0], 10);
-const tree = new BinaryTree();
-
-for (let i = 1; i <= n; i++) {
-  const [value, left, right] = input[i].split(" ");
-  tree.addNode(value, left, right);
+function postOrder(node) {
+  if (node === ".") return;
+  const [left, right] = tree[node];
+  postOrder(left);
+  postOrder(right);
+  result += node;
 }
 
-console.log(tree.preOrder(tree.nodes["A"]).join(""));
-console.log(tree.inOrder(tree.nodes["A"]).join(""));
-console.log(tree.postOrder(tree.nodes["A"]).join(""));
+preOrder("A");
+result += "\n";
+inOrder("A");
+result += "\n";
+postOrder("A");
+result += "";
+console.log(result);
