6509f2320c6a2cb2c6ecc9a1366f9e65bca4fe65
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2024-12-15 15:43:03 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 92 ms, Memory: 9400 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const tryCount = Number(input.shift());
+const result = [];
+
+for (let i = tryCount; i >= 1; i--) {
+  const star = "*".repeat(2 * i - 1);
+  const space = " ".repeat(tryCount - i);
+
+  result.push(space + star);
+}
+
+console.log(result.join("\n"));
