500cb9d609a79e4e2f03ef5706b303104f27ac14
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2025-02-26 21:43:26 +0900

    [Bronze II] Title: 나머지, Time: 92 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.js
+++ 백준/Bronze/3052. 나머지/나머지.js
@@ -1,22 +1,6 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
-const input = fs
-  .readFileSync(filePath)
-  .toString()
-  .trim()
-  .split("\n")
-  .map(Number);
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-let newArr = [];
-for (let i = 0; i < input.length; i++) {
-  newArr.push(input[i] % 42);
-}
-
-let result = [];
-for (let j = 0; j < newArr.length; j++) {
-  if (!result.includes(newArr[j])) {
-    result.push(newArr[j]);
-  }
-}
-
-console.log(result.length);
+const numbers = input.map((value) => value % 42);
+console.log(new Set(numbers).size);
