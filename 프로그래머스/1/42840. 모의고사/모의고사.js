ea8add80c3e22cba709da0343bdac6e1d6d2016c
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2025-02-28 15:50:08 +0900

    [level 1] Title: 모의고사, Time: 5.11 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -0,0 +1,26 @@
+function solution(answers) {
+  const answer = [0, 0, 0]; // 점수 score를 담을 배열
+
+  // 수포자들의 찍는 방식
+  const supo1 = [1, 2, 3, 4, 5];
+  const supo2 = [2, 1, 2, 3, 2, 4, 2, 5];
+  const supo3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+
+  for (let i = 0; i < answers.length; i++) {
+    if (answers[i] === supo1[i % supo1.length]) answer[0]++;
+    if (answers[i] === supo2[i % supo2.length]) answer[1]++;
+    if (answers[i] === supo3[i % supo3.length]) answer[2]++;
+  }
+
+  // 가장 많이 맞춘 사람을 찾는다.
+  const maxScore = Math.max(...answer);
+  const result = [];
+
+  for (let i = 0; i < answer.length; i++) {
+    if (answer[i] === maxScore) {
+      result.push(i + 1);
+    }
+  }
+
+  return result.sort((a, b) => a - b);
+}
