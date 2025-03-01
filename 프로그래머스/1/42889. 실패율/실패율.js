c0985d8944d5a0fa4658ab95c63edbb6d10e6642
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2025-03-01 13:47:41 +0900

    [level 1] Title: 실패율, Time: 4953.41 ms, Memory: 67.2 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -6,13 +6,15 @@ function solution(N, stages) {
     const clear = stages.filter((value) => value >= i).length; // 스테이지에 도달한 총 플레이어 수
     const failRate = notClear / clear; // 실패율 계산
 
-    answer.push([i, failRate]);
+    answer.push({ stage: i, rate: failRate });
   }
 
   answer.sort((a, b) => {
-    if (a[0] === b[0]) return a[1] - b[1];
-    else return b[1] - a[1];
+    // 실패율이 같으면 작은 번호의 스테이지가 앞에 온다.
+    if (a.rate === b.rate) return a.stage - b.stage;
+    // 그 외의 경우 실패율이 높은 스테이지가 앞에 온다.
+    else return b.rate - a.rate;
   });
 
-  return answer.map((value) => value[0]);
+  return answer.map((value) => value.stage);
 }
