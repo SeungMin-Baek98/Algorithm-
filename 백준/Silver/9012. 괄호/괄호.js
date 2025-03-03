f795df3e2d1f7828497b8cdaf86dde80aea9b596
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2025-03-03 14:45:39 +0900

    [Silver IV] Title: 괄호, Time: 104 ms, Memory: 9536 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.js
+++ 백준/Silver/9012. 괄호/괄호.js
@@ -2,28 +2,24 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-function solution(s) {
-  let answer = "YES";
-  let stack = [];
-
-  //괄호 반복
-  for (let x of s) {
-    if (x === "(") {
-      stack.push(x);
-    } else {
-      //닫는 괄호가 더 많을 경우
-      if (stack.length === 0) return "NO";
+function solution(arr) {
+  const stack = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "(") {
+      stack.push(arr[i]);
+    } else if (arr[i] === ")") {
+      if (stack.length === 0) {
+        return "NO";
+      }
       stack.pop();
     }
   }
-  //여는 괄호가 더 많을 경우
-  if (stack.length > 0) answer = "NO";
 
-  return answer;
+  return stack.length === 0 ? "YES" : "NO";
 }
 
-const tryCount = Number(input.shift());
+const testCase = Number(input.shift());
 
-for (let i = 0; i < tryCount; i++) {
+for (let i = 0; i < testCase; i++) {
   console.log(solution(input[i]));
 }
