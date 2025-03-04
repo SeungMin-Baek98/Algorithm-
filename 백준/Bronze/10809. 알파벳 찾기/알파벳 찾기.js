db16e3bdc598d5d528b938be2e92c0735729266a
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2025-03-04 17:31:48 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 104 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.js
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.js
@@ -1,11 +1,13 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
-const input = fs.readFileSync(filePath).toString();
+const input = fs.readFileSync(filePath).toString().trim().split("");
 
-const result = [];
-
-for (let i = "a".charCodeAt(); i <= "z".charCodeAt(); i++) {
-  result.push(input.indexOf(String.fromCharCode(i)));
+const alphabet = Array(26).fill(-1);
+for (let i = 0; i < input.length; i++) {
+  const idx = input[i].charCodeAt() - 97;
+  if (alphabet[idx] === -1) {
+    alphabet[idx] = i;
+  }
 }
 
-console.log(result.join(" "));
+console.log(alphabet.join(" "));
