f4ca12c700c15047c825c45a868867b5169d61af
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2025-03-12 14:11:29 +0900

    [Silver V] Title: 나이순 정렬, Time: 384 ms, Memory: 48860 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
@@ -2,8 +2,15 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const tryCount = input.shift();
+const N = Number(input.shift());
 
-input.sort((a, b) => a.split(" ")[0] - b.split(" ")[0]);
+const members = input.map((member) => member.split(" "));
+members.sort((a, b) => {
+  if (a[0] === b[0]) {
+    return a[1] - b[1]; // 나이가 같으면 가입한 순으로 정렬
+  } else {
+    return a[0] - b[0]; // 나머지는 나이순으로 정렬
+  }
+});
 
-console.log(input.join("\n"));
+console.log(members.map((member) => member.join(" ")).join("\n"));
