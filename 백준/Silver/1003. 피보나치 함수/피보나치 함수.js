2fde5201c1aca998dd64c2deeaf6b9604f7c11e7
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2025-04-09 13:55:49 +0900

    [Silver III] Title: 피보나치 함수, Time: 96 ms, Memory: 10140 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.js
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.js
@@ -2,30 +2,24 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const tryCount = Number(input.shift());
+function solution(N) {
+  let dp = Array.from({ length: N }, () => [0, 0]);
 
-function fibonnaci(n) {
-  let zero = 0;
-  let one = 0;
+  dp[0] = [1, 0]; // f(0) -> 1 f(1) -> 0
+  dp[1] = [0, 1]; // f(0) -> 0 f(1) -> 1
 
-  let dy = Array.from({ length: n });
-
-  dy[0] = 0;
-  dy[1] = 1;
-
-  for (let i = 2; i <= n; i++) {
-    dy[i] = dy[i - 2] + dy[i - 1];
+  for (let i = 2; i <= N; i++) {
+    dp[i] = [
+      dp[i - 2][0] + dp[i - 1][0], // 0의 호출 횟수
+      dp[i - 2][1] + dp[i - 1][1], // 1의 호출 횟수
+    ];
   }
 
-  zero = dy[n - 1] === undefined ? 1 : dy[n - 1];
-  one = dy[n];
-
-  let answer = [];
-  answer.push(zero, one);
-
-  return answer.join(" ");
+  return dp[N];
 }
 
-for (let i = 0; i < tryCount; i++) {
-  console.log(fibonnaci(input[i]));
+const testCase = Number(input.shift());
+
+for (let i = 0; i < testCase; i++) {
+  console.log(solution(input[i]).join(" "));
 }
