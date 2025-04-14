9d87a3fe96ba48206a9f593e7e39a07d5e32573c
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2025-04-14 15:16:50 +0900

    [Silver III] Title: 계단 오르기, Time: 92 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.js
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.js
@@ -1,23 +1,22 @@
+// 백준 2579
+// 계단 오르기
+
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const stairs = Number(input.shift());
-const scores = [0, ...input.map(Number)];
+const N = Number(input.shift());
+const stairs = input.map(Number);
 
-function solution(stairs, scores) {
-  let dy = Array.from({ length: stairs + 1 }, () => 0);
+let dp = Array.from({ length: N + 1 }).fill(0);
+dp[1] = stairs[0]; // 1계단 올랐을 때 최대값
+dp[2] = stairs[0] + stairs[1]; // 2계단 올랐을 때 최대값.
 
-  dy[1] = scores[1];
-  dy[2] = scores[1] + scores[2];
-
-  for (let i = 3; i <= stairs; i++) {
-    dy[i] = Math.max(
-      dy[i - 2] + scores[i],
-      dy[i - 3] + scores[i - 1] + scores[i]
-    );
-  }
-  return dy[stairs];
+for (let i = 3; i <= N; i++) {
+  dp[i] = Math.max(
+    dp[i - 2] + stairs[i - 1], // 한 게단 오를 경우
+    dp[i - 3] + stairs[i - 2] + stairs[i - 1] // 두 계단 오를 경우
+  );
 }
 
-console.log(solution(stairs, scores));
+console.log(dp[N]);
