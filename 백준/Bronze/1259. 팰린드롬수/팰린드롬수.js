6b7a3d23991f5d3cfe9968209ab6f48c8e3d9453
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2025-04-15 12:59:52 +0900

    [Bronze I] Title: 팰린드롬수, Time: 96 ms, Memory: 9412 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.js
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.js
@@ -1,26 +1,29 @@
+// 백준 1259
+// 팰린드롬수
+
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-function isPalindrome(num) {
+function isFalindrome(arr) {
   let left = 0;
-  let right = num.length - 1;
+  let right = arr.length - 1;
 
   while (left < right) {
-    if (num[left] !== num[right]) {
+    if (arr[left] !== arr[right]) {
       return false;
     }
     left++;
     right--;
   }
-
   return true;
 }
 
 for (let i = 0; i < input.length; i++) {
-  if (input[i] === "0") break;
-  else {
-    if (isPalindrome(input[i])) {
+  if (input[i] === "0") {
+    break;
+  } else {
+    if (isFalindrome(input[i])) {
       console.log("yes");
     } else {
       console.log("no");
