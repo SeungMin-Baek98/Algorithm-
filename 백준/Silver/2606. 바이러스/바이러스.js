7b600af2b26dd633b1d67c694b461695e7750416
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2025-04-15 13:38:46 +0900

    [Silver III] Title: 바이러스, Time: 92 ms, Memory: 9480 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.js
+++ 백준/Silver/2606. 바이러스/바이러스.js
@@ -1,32 +1,35 @@
+// 백준 1991
+// 바이러스
+
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const n = Number(input[0]);
-const edges = input.slice(2).map((v) => v.split(" ").map(Number));
+const graphSize = Number(input.shift()); // 7
+const graph = Array.from({ length: graphSize + 1 }, () =>
+  Array(graphSize + 1).fill(0)
+);
+const edges = Number(input.shift()); // 6
+const nodes = input.map((value) => value.split(" ").map(Number)); // 간선 정보
+const isVisited = Array(graphSize + 1).fill(false);
 
-function solution(n, arr) {
-  let answer = 0;
-  let graph = Array.from(Array(n + 1), () => Array(n + 1).fill(0));
-  let isVisited = Array(n + 1).fill(false);
-
-  for (let [a, b] of arr) {
-    graph[a][b] = 1;
-    graph[b][a] = 1;
-  }
-
-  function DFS(node) {
-    isVisited[node] = true;
-
-    for (let i = 1; i <= n; i++) {
-      if (graph[node][i] === 1 && !isVisited[i]) {
-        answer++;
-        DFS(i);
-      }
-    }
-  }
-  DFS(1);
-  return answer;
+for (let [start, end] of nodes) {
+  graph[start][end] = 1;
+  graph[end][start] = 1;
 }
 
-console.log(solution(n, edges));
+let count = 0;
+
+function dfs(node) {
+  isVisited[node] = true;
+  count++;
+
+  for (let i = 1; i <= graphSize; i++) {
+    if (graph[node][i] === 1 && !isVisited[i]) {
+      dfs(i);
+    }
+  }
+}
+
+dfs(1);
+console.log(count - 1);
