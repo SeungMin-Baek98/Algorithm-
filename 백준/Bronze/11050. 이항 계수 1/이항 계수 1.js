5208d85140be0d985e237a54770c0c02b3f50918
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2025-04-23 13:45:19 +0900

    [Bronze I] Title: 이항 계수 1, Time: 96 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.js
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.js
@@ -1,16 +1,20 @@
+// 백준 11050
+// 이항 계수
+
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split(" ");
 
-const [N, K] = input.map(Number);
 function factorial(n) {
-  let answer = 1;
-  for (let i = 1; i <= n; i++) {
-    answer *= i;
-  }
+  let mul = 1;
 
-  return answer;
+  for (let i = n; i >= 1; i--) {
+    mul *= i;
+  }
+  return mul;
 }
 
-const result = factorial(N) / (factorial(N - K) * factorial(K));
+const [N, K] = input.map(Number);
+const result = factorial(N) / (factorial(K) * factorial(N - K));
+
 console.log(result);
