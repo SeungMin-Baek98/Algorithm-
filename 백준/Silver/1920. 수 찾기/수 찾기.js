34cfe5af987653fa86f69c29c71c487b117ef4c2
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2025-05-08 13:46:53 +0900

    [Silver IV] Title: 수 찾기, Time: 244 ms, Memory: 39360 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.js
+++ 백준/Silver/1920. 수 찾기/수 찾기.js
@@ -1,18 +1,23 @@
+// 백준 1018
+// 체스판 다시 칠하기
+
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const numbers = new Set(input[1].split(" ").map(Number));
-const findNumbers = input[3].split(" ").map(Number);
+const N = Number(input.shift());
+const numbers = input.shift().split(" ").map(Number);
 
-let result = [];
-
-for (let i = 0; i < findNumbers.length; i++) {
-  if (numbers.has(findNumbers[i])) {
-    result.push(1);
-  } else {
-    result.push(0);
-  }
+const map = new Map();
+for (let i = 0; i < N; i++) {
+  map.set(numbers[i], 1);
 }
 
-console.log(result.join("\n"));
+const M = Number(input.shift());
+const findNums = input.shift().split(" ").map(Number);
+
+for (let i = 0; i < M; i++) {
+  findNums[i] = map.get(findNums[i]) ? 1 : 0;
+}
+
+console.log(findNums.join("\n"));
