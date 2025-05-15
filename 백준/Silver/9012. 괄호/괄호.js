3913a98520398faf00278cda359dc3b5d0fe0177
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2025-05-15 13:45:30 +0900

    [Silver IV] Title: 괄호, Time: 100 ms, Memory: 9616 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.js
+++ 백준/Silver/9012. 괄호/괄호.js
@@ -1,25 +1,32 @@
+// 백준 9012
+// 괄호
+
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-function solution(arr) {
+const N = Number(input.shift());
+
+for (let strs of input) {
   const stack = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === "(") {
-      stack.push(arr[i]);
-    } else if (arr[i] === ")") {
+  let isValid = true;
+
+  for (let x of strs) {
+    if (x === "(") {
+      stack.push(x);
+    } else if (x === ")") {
       if (stack.length === 0) {
-        return "NO";
+        isValid = false;
+        break;
       }
+
       stack.pop();
     }
   }
 
-  return stack.length === 0 ? "YES" : "NO";
-}
-
-const testCase = Number(input.shift());
-
-for (let i = 0; i < testCase; i++) {
-  console.log(solution(input[i]));
+  if (isValid && stack.length === 0) {
+    console.log("YES");
+  } else {
+    console.log("NO");
+  }
 }
