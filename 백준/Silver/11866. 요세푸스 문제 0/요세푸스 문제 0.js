66dfe40a5f4f6bc5e682173cff6f84455350cef1
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2025-05-26 14:05:47 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 184 ms, Memory: 11792 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.js
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.js
@@ -1,15 +1,20 @@
+// 백준 11866
+// 요세푸스 문제 0
+
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split(" ");
 
-const [n, k] = input;
-let numbers = Array.from({ length: n }, (_, i) => i + 1);
-let result = [];
+const [N, K] = input.map(Number);
+const arr = Array.from({ length: N }, (_, i) => i + 1);
+const result = [];
 
-while (numbers.length > 0) {
-  for (let i = 0; i < k - 1; i++) {
-    numbers.push(numbers.shift());
+while (arr.length) {
+  for (let i = 0; i < K - 1; i++) {
+    arr.push(arr.shift());
   }
-  result.push(numbers.shift());
+
+  result.push(arr.shift());
 }
+
 console.log(`<${result.join(", ")}>`);
