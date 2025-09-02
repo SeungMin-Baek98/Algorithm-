1d271072671c18611e6a91dd289616ca3fbcd431
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2025-09-02 15:59:35 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 2.23 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -0,0 +1,13 @@
+function solution(arr) {
+  let result = 1;
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i][j] !== arr[j][i]) {
+        result = 0;
+      }
+    }
+  }
+
+  return result;
+}
