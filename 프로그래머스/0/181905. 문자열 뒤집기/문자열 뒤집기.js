9a69a9956e5c061067f30e88792bc69594f21fe1
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2025-09-11 13:34:28 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.js
@@ -0,0 +1,12 @@
+function solution(my_string, s, e) {
+  var answer = "";
+
+  const sliceStr = my_string
+    .slice(s, e + 1)
+    .split("")
+    .reverse()
+    .join("");
+
+  answer = my_string.slice(0, s) + sliceStr + my_string.slice(e + 1);
+  return answer;
+}
