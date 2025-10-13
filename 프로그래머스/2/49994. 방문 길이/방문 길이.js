2d8c51db49b96d38be89b6d3a210b5572d16d7bf
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2025-10-13 09:28:29 +0900

    [level 2] Title: 방문 길이, Time: 0.67 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -1,43 +1,27 @@
+// LV.2 방문 길이
 function solution(dirs) {
-  const U = [0, 1]; // 위쪽
-  const D = [0, -1]; // 아래쪽
-  const R = [1, 0]; // 오른쪽
-  const L = [-1, 0]; //왼쪽
-
-  // 중복된 경로를 제거하기 위한 Set 생성자 생성
+  const move = {
+    U: [0, 1],
+    D: [0, -1],
+    R: [1, 0],
+    L: [-1, 0],
+  };
   const visited = new Set();
-
-  // 시작점 (0,0)
-  let nowRoute = [0, 0];
+  let start = [0, 0];
 
   for (let i = 0; i < dirs.length; i++) {
-    let end;
+    const [dx, dy] = move[dirs[i]];
+    const nx = start[0] + dx;
+    const ny = start[1] + dy;
 
-    switch (dirs[i]) {
-      case "U":
-        end = [nowRoute[0] + U[0], nowRoute[1] + U[1]];
-        break;
-      case "D":
-        end = [nowRoute[0] + D[0], nowRoute[1] + D[1]];
-        break;
-      case "R":
-        end = [nowRoute[0] + R[0], nowRoute[1] + R[1]];
-        break;
-      case "L":
-        end = [nowRoute[0] + L[0], nowRoute[1] + L[1]];
-        break;
-    }
+    if (nx > 5 || nx < -5 || ny > 5 || ny < -5) continue;
 
-    // 범위를 벗어나면 이동하지 않음
-    if (end[0] < -5 || end[0] > 5 || end[1] < -5 || end[1] > 5) continue;
-
-    // 중복된 경로를 제거하기 위해 Set에 경로 추가
-    visited.add(`${nowRoute[0]}${nowRoute[1]}${end[0]}${end[1]}`);
-    visited.add(`${end[0]}${end[1]}${nowRoute[0]}${nowRoute[1]}`);
-
-    // nowRoute를 end로 업데이트하여 위치 이동
-    nowRoute = end;
+    visited.add(`${start[0]},${start[1]}-${nx},${ny}`);
+    visited.add(`${nx},${ny}-${start[0]},${start[1]}`);
+    start[0] = nx;
+    start[1] = ny;
   }
 
-  return visited.size / 2;
+  var answer = visited.size /2 ;
+  return answer;
 }
