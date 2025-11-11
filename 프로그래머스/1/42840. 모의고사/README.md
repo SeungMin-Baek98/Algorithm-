45ff89b7c27581c3dab852768ddc0fdb482d545a
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2025-11-11 15:42:41 +0900

    [level 1] Title: 모의고사, Time: 3.81 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 36.8 MB, 시간: 3.64 ms
+메모리: 36.7 MB, 시간: 3.81 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 28일 15:53:32
+2025년 11월 11일 15:42:36
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,26 +1,27 @@
 function solution(answers) {
-  const answer = [0, 0, 0]; // 점수 score를 담을 배열
+  // 반복되는 처음과 끝을 기준 배열을 생성하여
+  // answers 배열을 순회하면서
+  // 각각의 맞춘 score를 일치하면은 ++ 해서
+  // Math.max()를 하면된다.
+  const vesicle1 = [1, 2, 3, 4, 5];
+  const vesicle2 = [2, 1, 2, 3, 2, 4, 2, 5];
+  const vesicle3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+  const answer = [];
 
-  // 수포자들의 찍는 방식
-  const supo1 = [1, 2, 3, 4, 5];
-  const supo2 = [2, 1, 2, 3, 2, 4, 2, 5];
-  const supo3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+  let count1 = 0;
+  let count2 = 0;
+  let count3 = 0;
 
   for (let i = 0; i < answers.length; i++) {
-    if (answers[i] === supo1[i % supo1.length]) answer[0]++;
-    if (answers[i] === supo2[i % supo2.length]) answer[1]++;
-    if (answers[i] === supo3[i % supo3.length]) answer[2]++;
+    if (answers[i] === vesicle1[i % vesicle1.length]) count1++;
+    if (answers[i] === vesicle2[i % vesicle2.length]) count2++;
+    if (answers[i] === vesicle3[i % vesicle3.length]) count3++;
   }
+  const maxScore = Math.max(count1, count2, count3);
 
-  // 가장 많이 맞춘 사람을 찾는다.
-  const maxScore = Math.max(...answer);
-  const result = [];
+  if (count1 === maxScore) answer.push(1);
+  if (count2 === maxScore) answer.push(2);
+  if (count3 === maxScore) answer.push(3);
 
-  for (let i = 0; i < answer.length; i++) {
-    if (answer[i] === maxScore) {
-      result.push(i + 1); // 1부터 시작하므로.
-    }
-  }
-
-  return result;
+  return answer;
 }
