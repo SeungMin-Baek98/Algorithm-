4ff343df0abe67e8499a897d4916320045746ae5
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2025-11-19 17:26:58 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.66 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,24 +1,35 @@
 function solution(board, moves) {
-  const n = board.length;
-  const stack = [];
-  var answer = 0;
+  // 1. 각 열에 대한 스택을 생성 (2차원)
+  const lanes = [...Array(board[0].length)].map(() => []);
 
-  // Moves순회
-  for (let i = 0; i < moves.length; i++) {
-    const c = moves[i] - 1; // [0,4,2,4,0,1,0,4]
+  // 2. board를 역순으로 탐개하며, 각 열의 인형을 lanes에 추가
+  for (let i = board.length - 1; i >= 0; i--) {
+    for (let j = 0; j < board[0].length; j++) {
+      if (board[i][j]) {
+        lanes[j].push(board[i][j]);
+      }
+    }
+  }
 
-    for (let r = 0; r < n; r++) {
-      if (board[r][c] !== 0) {
-        const doll = board[r][c];
-        board[r][c] = 0;
+  // 3. 인형을 담을 bucket을 생성
+  const bucket = [];
 
-        if (stack[stack.length - 1] === doll) {
-          stack.pop();
-          answer += 2;
-        } else {
-          stack.push(doll);
-        }
-        break;
+  // 4. 사라진 인형의 총 개수를 저장할 변수 초기화
+  let answer = 0;
+
+  // 5. moves를 순회하며, 각 열에서 인형을 뽑아 bucket에 추가
+  for (const m of moves) {
+    if (lanes[m - 1].length > 0) {
+      // 해당 열에 인형이 있는 경우
+      const doll = lanes[m - 1].pop();
+
+      if (bucket.length > 0 && bucket[bucket.length - 1] === doll) {
+        // 6. 바구니에 인형이 있고, 가장 위에 있는 인형과 같은 경우
+        bucket.pop();
+        answer += 2;
+      } else {
+        // 7. 바구니에 인형이 없거나, 가장 위에 있는 인형과 다른 경우
+        bucket.push(doll);
       }
     }
   }
