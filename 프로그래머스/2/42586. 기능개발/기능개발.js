cfbb7628be938213c2d221d7658b0c45f73efc65
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2025-11-20 17:12:24 +0900

    [level 2] Title: 기능개발, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,23 +1,20 @@
-
 function solution(progresses, speeds) {
-  const days = progresses.map((progress, idx) =>
-    Math.ceil((100 - progress) / speeds[idx])
-  );
-
-  const answer = [];
+  var answer = [];
+  const days = progresses.map((progess, idx) => {
+    return Math.ceil((100 - progess) / speeds[idx]);
+  });
   let maxDay = days[0];
   let count = 1;
-
   for (let i = 1; i < days.length; i++) {
-    if (maxDay >= days[i]) {
+    if (days[i] <= maxDay) {
       count++;
     } else {
       answer.push(count);
-      count = 1; // 다시 초기화를 함으로서 다음 기능의 개발 속도 세기위함
-      maxDay = days[i]; // maxDay를 다음 기능의 개발 속도로 변경
+      maxDay = days[i];
+      count = 1;
     }
   }
-  answer.push(count); // 마지막 기능의 개발 속도를 answer에 push
 
+  answer.push(count);
   return answer;
 }
