6b102a3835ce33c5d2ef4cdcafcc8fad707e918b
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2025-12-15 16:05:13 +0900

    [level 2] Title: 짝지어 제거하기, Time: 69.33 ms, Memory: 57.8 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,15 +1,13 @@
 function solution(s) {
-  let answer = 0;
-
   const stack = [];
-  for (const char of s) {
-    if (char !== stack[stack.length - 1]) {
-      stack.push(char);
+
+  for (const str of s) {
+    if (str !== stack[stack.length - 1]) {
+      stack.push(str);
     } else {
       stack.pop();
     }
   }
 
-  answer = stack.length === 0 ? 1 : 0;
-  return answer;
+  return stack.length === 0 ? 1 : 0;
 }
