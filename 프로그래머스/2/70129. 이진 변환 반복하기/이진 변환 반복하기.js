fb029ade36d1597d20e61f8ee5fb8bf80e33ffa1
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2026-03-14 21:36:46 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 14.59 ms, Memory: 41.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -0,0 +1,20 @@
+function solution(s) {
+  let transformCount = 0; // 몇번 변환됐는지 카운트
+  let removedZeroCOunt = 0; // 0 횟수 카운트
+
+  // s의 길이가 1될떄까지 while
+  while (s !== "1") {
+    const zeroCount = s.split("").filter((item) => item === "0").length;
+    removedZeroCOunt += zeroCount;
+
+    const filtered = s
+      .split("")
+      .filter((item) => item === "1")
+      .join("");
+
+    s = filtered.length.toString(2); // 2진법 변환을 위해 toString() 사용
+    transformCount++;
+  }
+
+  return [transformCount, removedZeroCOunt];
+}
