254b29636fc2ecf01bd7fe6b519fd8290f7c5d6e
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2026-03-15 16:29:32 +0900

    [level 2] Title: 숫자의 표현, Time: 2.48 ms, Memory: 36.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,20 @@
+function solution(n) {
+  let answer = 0;
+
+  for (let i = 1; i < n; i++) {
+    let sum = 0;
+    for (let j = i; j < n; j++) {
+      sum += j;
+
+      if (sum === n) {
+        answer++;
+        break;
+      }
+
+      if (sum + j > n) {
+        break;
+      }
+    }
+  }
+  return answer + 1;
+}
