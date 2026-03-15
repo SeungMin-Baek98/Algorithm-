ccaef93ca6f38687599699c77eca90fcee286d63
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2026-03-15 18:16:56 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,11 +1,35 @@
+// function solution(s) {
+//   return s
+//     .split(" ")
+//     .map((word) =>
+//       word
+//         .split("")
+//         .map((ch, idx) => (idx % 2 === 0 ? ch.toUpperCase() : ch.toLowerCase()))
+//         .join(""),
+//     )
+//     .join(" ");
+// }
 function solution(s) {
-  return s
-    .split(" ")
-    .map((word) =>
-      word
-        .split("")
-        .map((ch, idx) => (idx % 2 === 0 ? ch.toUpperCase() : ch.toLowerCase()))
-        .join(""),
-    )
-    .join(" ");
+  let result = "";
+  const word = s.split(" "); // 공백으로 split
+
+  for (let i = 0; i < word.length; i++) {
+    const wordSplit = word[i].split(""); // 단어 split
+
+    for (let j = 0; j < wordSplit.length; j++) {
+      if (j % 2 === 0) {
+        wordSplit[j] = wordSplit[j].toUpperCase();
+      } else {
+        wordSplit[j] = wordSplit[j].toLowerCase();
+      }
+
+      result += wordSplit[j];
+    }
+
+    if (i !== word.length - 1) {
+      result += " ";
+    }
+  }
+
+  return result;
 }
