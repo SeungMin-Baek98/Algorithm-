c9caee9abca99ba909e9ef61a9cdcf7729b40ca7
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2026-06-24 13:23:50 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.13 ms, Memory: 43.9 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -1,14 +1,25 @@
-function solution(s) {
-  let answer = 0;
-  const arr = s.split(" ");
+// function solution(s) {
+//   let answer = 0;
+//   const arr = s.split(" ");
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === "Z") {
-      answer -= +arr[i - 1];
-    } else {
-      answer += +arr[i];
-    }
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === "Z") {
+//       answer -= +arr[i - 1];
+//     } else {
+//       answer += +arr[i];
+//     }
+//   }
+
+//   return answer;
+// }
+
+
+function solution(s) {
+  const arr = [];
+  const str = s.split(" ");
+  for (let i = 0; i < str.length; i++) {
+    str[i] === "Z" ? arr.pop() : arr.push(+str[i]);
   }
 
-  return answer;
+  return arr.length ? arr.reduce((acc, cur) => acc + cur) : 0;
 }
