c343f53ee29eee4aa3015f647b8611894b9673f6
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2026-07-08 13:52:47 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 114.64 ms, Memory: 95.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,48 +1,16 @@
-// function solution(participant, completion) {
-//   let obj = {};
-
-//   // 참가자 명단을 객체로 만들어서 value값에 1씩 더해준다.
-//   for (let i = 0; i < participant.length; i++) {
-//     if (obj[participant[i]]) {
-//       obj[participant[i]]++;
-//     } else {
-//       obj[participant[i]] = 1;
-//     }
-//   }
-
-//   // 완주자 명단을 객체로 만들어서 value값에 이름이 있을경우 완주하였으므로 1씩 뺴준다.
-//   for (let i = 0; i < completion.length; i++) {
-//     if (obj[completion[i]]) {
-//       obj[completion[i]]--;
-//     }
-//   }
-
-//   for (let key in obj) {
-//     if (obj[key] !== 0) {
-//       return key;
-//     }
-//   }
-// }
-
-
 function solution(participant, completion) {
-  const map = new Map();
+  let obj = {};
 
   for (let i = 0; i < participant.length; i++) {
-    if (map.has(participant[i])) {
-      map.set(participant[i], map.get(participant[i]) + 1);
-    } else {
-      map.set(participant[i], 1);
-    }
+    obj[participant[i]] = (obj[participant[i]] || 0) + 1;
   }
 
   for (let i = 0; i < completion.length; i++) {
-    map.set(completion[i], map.get(completion[i]) - 1);
+    obj[completion[i]] = obj[completion[i]] - 1;
   }
 
-  for (let [key, value] of map) {
-    if (value > 0) {
-      return key;
-    }
-  }
+  return Object.entries(obj)
+    .filter(([name, count]) => count > 0)
+    .map(([name, count]) => [name])
+    .join("");
 }
