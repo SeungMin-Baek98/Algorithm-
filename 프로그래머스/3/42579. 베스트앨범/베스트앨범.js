0dc6caccb0421a6b4ff533b7568bfce8ab0de39c
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2026-07-14 14:12:14 +0900

    [level 3] Title: 베스트앨범, Time: 0.36 ms, Memory: 44.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -1,41 +1,27 @@
-
 function solution(genres, plays) {
-  var answer = [];
   const obj = {};
-
+  const songs = [];
+  const answer = [];
   for (let i = 0; i < genres.length; i++) {
     const genre = genres[i];
-    const play = plays[i];
+    obj[genre] = (obj[genre] || 0) + plays[i];
 
-    if (obj[genre]) {
-      obj[genre] += play;
-    } else {
-      obj[genre] = play;
-    }
+    songs.push({ genre, play: plays[i], index: i });
   }
 
-  const sortedGenres = Object.entries(obj).sort((a, b) => b[1] - a[1]); // 재생 횟수를 기준으로 정렬
+  const sortedGenre = Object.entries(obj).sort((a, b) => b[1] - a[1]);
 
-  for (const [genre] of sortedGenres) {
-    const songs = [];
+  for (const [genre] of sortedGenre) {
+    const genreSongs = songs
+      .filter((song) => song.genre === genre)
+      .sort((a, b) => {
+        if (a.play === b.play) return a.play - b.play;
+        else return b.play - a.play;
+      })
+      .slice(0, 2);
 
-    for (let i = 0; i < genres.length; i++) {
-      if (genres[i] === genre) {
-        songs.push([i, plays[i]]);
-      }
-    }
-
-    songs.sort((a, b) => {
-      if (b[1] === a[1]) {
-        return a[0] - b[0]; // 재생 횟수가 같으면 고유 번호가 낮은 순서로 정렬
-      }
-      return b[1] - a[1]; // 재생 횟수 내림차순 정렬
-    });
-
-    answer.push(songs[0][0]); // 가장 많이 재생된 노래 추가
-    if (songs.length > 1) {
-      // 두 번째로 많이 재생된 노래 추가
-      answer.push(songs[1][0]);
+    for (const song of genreSongs) {
+      answer.push(song.index);
     }
   }
 
