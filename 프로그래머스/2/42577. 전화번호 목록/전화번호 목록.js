0b5ec95613cd6dc9237e22454d4f509d0178763c
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2026-08-19 12:14:48 +0900

    [level 2] Title: 전화번호 목록, Time: 112.94 ms, Memory: 97.3 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,10 +1,8 @@
 function solution(phone_book) {
-  phone_book.sort(); // 정렬을 해두면 양 옆 번호들의 접두어가 똑같은게 위치한다.
+  phone_book.sort(); // sort()메서드를 통하여 접두어가 비슷한 단어들이 양 옆에 오도록 설정
 
-  // 마지막 요소가 현재가 될 필요는 없다. 그래서 배열 길이의 -1 까지 순회를 해준다.
   for (let i = 0; i < phone_book.length - 1; i++) {
     if (phone_book[i + 1].startsWith(phone_book[i])) return false;
   }
-
   return true;
 }
