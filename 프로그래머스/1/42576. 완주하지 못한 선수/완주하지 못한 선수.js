c8f01438256352a6de4abcb23145b4a6c6fec692
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2026-08-19 12:40:24 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 115.90 ms, Memory: 94.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,16 +1,17 @@
 function solution(participant, completion) {
-  let obj = {};
+  const people = {};
 
   for (let i = 0; i < participant.length; i++) {
-    obj[participant[i]] = (obj[participant[i]] || 0) + 1;
+    people[participant[i]] = (people[participant[i]] || 0) + 1;
   }
 
   for (let i = 0; i < completion.length; i++) {
-    obj[completion[i]] = obj[completion[i]] - 1;
+    people[completion[i]] = people[completion[i]] - 1;
   }
-
-  return Object.entries(obj)
+  return Object.entries(people)
     .filter(([name, count]) => count > 0)
-    .map(([name, count]) => [name])
+    .map(([name, count]) => {
+      return name;
+    })
     .join("");
 }
