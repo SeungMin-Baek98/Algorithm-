b028134706bd141bf735dc95e698e0d3d59cdb06
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2026-08-19 13:11:48 +0900

    [level 2] Title: 의상, Time: 0.15 ms, Memory: 43.8 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,12 +1,19 @@
 function solution(clothes) {
-  let obj = {};
+  const clothe = {};
 
   for (let i = 0; i < clothes.length; i++) {
-    const kind = clothes[i][1];
+    const [name, kind] = clothes[i];
 
-    obj[kind] = (obj[kind] || 0) + 1;
+    clothe[kind] = (clothe[kind] || 0) + 1;
   }
-  // 각 의상 종류에서 '입지 않는 경우'도 선택지에 포함하기 위해 cur + 1을 한다.
-  // 단, 최소 한 개의 의상은 입어야 하므로 '모든 의상을 입지 않는 경우' 1가지를 제외한다.
-  return Object.values(obj).reduce((acc, cur) => acc * (cur + 1), 1) - 1;
+  const clotheKindLength = Object.keys(clothe).length;
+
+  if (clotheKindLength === 1) return +Object.values(clothe).join();
+
+  const clotheTotal = Object.values(clothe).reduce(
+    (acc, cur) => acc * (cur + 1),
+    1,
+  );
+
+  return clotheTotal - 1;
 }
