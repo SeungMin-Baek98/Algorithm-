ae058fad892c24713f699d3e90db020f80baa6a9
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2026-08-20 13:21:03 +0900

    [level 1] Title: 폰켓몬, Time: 0.56 ms, Memory: 44.5 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,5 +1,8 @@
 function solution(nums) {
-  const setArr = new Set(nums);
+  const maxKind = nums.length / 2;
+  const removeDupli = [...new Set(nums)];
 
-  return setArr.size > nums.length / 2 ? Math.min(setArr.size, nums.length/2) : setArr.size;
+  if (removeDupli.length === maxKind) return maxKind;
+  else if (removeDupli.length < maxKind) return removeDupli.length;
+  else return maxKind;
 }
