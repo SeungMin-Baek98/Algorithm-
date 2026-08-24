06198d75118c41a96992ec34c30fc45ffab979e1
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2026-08-24 13:42:53 +0900

    [level 2] Title: 프로세스, Time: 0.99 ms, Memory: 44.8 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,22 +1,27 @@
+// 프로세스
+// 13:10
+// 13:40
 function solution(priorities, location) {
-  let answer = 0;
-  const queue = priorities.map((priority, index) => ({
-    priority,
-    index,
-  }));
+  const priorityArr = priorities.map((priority, index) => [index, priority]);
 
-  while (queue.length) {
-    const current = queue.shift();
-    if (queue.some((item) => item.priority > current.priority)) {
-      queue.push(current);
+  let count = 0;
+
+  while (priorityArr.length > 0) {
+    // 배열 첫번째 요소 shift()
+    const current = priorityArr.shift();
+    // 첫번째 idx보다 더 높은 우서순위가 있는지 비교
+    const hasHigherPrioirty = priorityArr.some(
+      ([idx, priority]) => priority > current[1],
+    );
+
+    if (hasHigherPrioirty) {
+      // 더 높은 우선순위가 있으면 배열 마지막 idx로 이동
+      priorityArr.push(current);
     } else {
-      answer++;
+      count++;
 
-      if (current.index === location) {
-        break;
-      }
+      // 찾는 위치랑 동일하다면 count반환
+      if (current[0] === location) return count;
     }
   }
-
-  return answer;
 }
