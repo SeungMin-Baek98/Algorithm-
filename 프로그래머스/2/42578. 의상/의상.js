068a99572e1750b63751e05eeadf5a83ec941a87
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2026-08-24 12:42:02 +0900

    [level 2] Title: 의상, Time: 0.21 ms, Memory: 44.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,19 +1,19 @@
 function solution(clothes) {
-  const clothe = {};
+  // 옷 종류 이름 보관할 객체 생성
+  const obj = {};
 
   for (let i = 0; i < clothes.length; i++) {
     const [name, kind] = clothes[i];
-
-    clothe[kind] = (clothe[kind] || 0) + 1;
+    // 같은 종류가 있으면은 누적합을 한다.
+    obj[kind] = (obj[kind] || 0) + 1;
   }
-  const clotheKindLength = Object.keys(clothe).length;
 
-  if (clotheKindLength === 1) return +Object.values(clothe).join();
+  const clothesKind = Object.keys(obj).length;
 
-  const clotheTotal = Object.values(clothe).reduce(
-    (acc, cur) => acc * (cur + 1),
-    1,
-  );
+  // 만일 옷 종류가 1개라면은 value값 return
+  if (clothesKind === 1) return +Object.values(obj).join("");
 
-  return clotheTotal - 1;
+  // 아니라면 각 (각 종류별 + 1 <- 안입는경우) 누적합 -1 (아에 안입는경우)
+
+  return Object.values(obj).reduce((acc, cur) => acc * (cur + 1), 1) - 1;
 }
