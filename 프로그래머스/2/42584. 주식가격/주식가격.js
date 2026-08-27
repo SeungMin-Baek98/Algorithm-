11903aafdc4b79f8a4d69f069f5f021286211900
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2026-08-27 13:16:09 +0900

    [level 2] Title: 주식가격, Time: 12.01 ms, Memory: 61.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,15 +1,14 @@
 function solution(prices) {
   const result = [];
-
   for (let i = 0; i < prices.length; i++) {
-    let seconds = 0;
+    let time = 0;
     for (let j = i + 1; j < prices.length; j++) {
-      seconds++;
+      time++;
 
-      if (prices[j] < prices[i]) break;
+      if (prices[i] > prices[j]) break;
     }
-
-    result.push(seconds);
+    result.push(time);
   }
+
   return result;
 }
