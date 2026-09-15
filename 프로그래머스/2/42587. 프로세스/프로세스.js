0f285412bc6c45e63b53ac422f80d96af65ddc78
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2026-09-15 13:35:08 +0900

    [level 2] Title: 프로세스, Time: 1.04 ms, Memory: 44.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,18 +1,14 @@
-function solution(priorites, location) {
-  // index를 가진 배열 생성
-  const priorityWithIdx = priorites.map((priority, index) => [index, priority]);
-
+function solution(priorities, location) {
+  const priorityArr = priorities.map((priority, index) => [index, priority]);
   let count = 0;
-
-  while (priorityWithIdx.length > 0) {
-    const current = priorityWithIdx.shift();
-
-    const hasHigherPriority = priorityWithIdx.some(
+  while (priorityArr.length > 0) {
+    const current = priorityArr.shift();
+    const hasHigherPriority = priorityArr.some(
       ([index, priority]) => current[1] < priority,
     );
 
     if (hasHigherPriority) {
-      priorityWithIdx.push(current);
+      priorityArr.push(current);
     } else {
       count++;
 
