80f4d3878d8c82f9cf3407a4e7c9ff17775ef615
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2026-09-15 13:15:48 +0900

    [level 2] Title: 기능개발, Time: 0.12 ms, Memory: 43.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,22 +1,23 @@
 function solution(progresses, speeds) {
-  // progresses를 활용하여 각 몇일이 걸리는지 days배열 생성
-  // Math.ceil() 사용 ->  "95%인 작업의 개발 속도가 하루에 4%라면 배포는 2일 뒤에 이루어집니다."
-  const days = progresses.map((progress, index) =>
-    Math.ceil((100 - progress) / speeds[index]),
+  const days = progresses.map((progress, idx) =>
+    Math.ceil((100 - progress) / speeds[idx]),
   );
-  const result = [];
-  let maxDays = days[0]; // 첫번째 날 기준
-  let count = 1; // 1일 기준
+  let result = [];
+  let maxDay = days[0];
+  let count = 1;
 
   for (let i = 1; i < days.length; i++) {
-    if (maxDays >= days[i]) {
+    const day = days[i];
+
+    if (maxDay >= day) {
       count++;
     } else {
       result.push(count);
-      maxDays = days[i];
+      maxDay = day;
       count = 1;
     }
   }
   result.push(count);
+
   return result;
 }
