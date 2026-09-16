51a6b11064a6353d794c08afbc0ac47fbc61d98c
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2026-09-16 12:25:11 +0900

    [level 2] Title: 가장 큰 수, Time: 110.96 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,7 +1,8 @@
 function solution(numbers) {
   const stringArr = numbers.map((number) => String(number));
-  const answer = stringArr
+  const sortArr = stringArr
     .sort((a, b) => Number(b + a) - Number(a + b))
     .join("");
-  return answer[0] === "0" ? "0" : answer;
+
+  return sortArr > 0 ? sortArr : "0";
 }
