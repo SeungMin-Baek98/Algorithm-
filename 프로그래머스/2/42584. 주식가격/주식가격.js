7370f9bfc2524b33dd51fd617885676494afae53
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2026-09-16 12:01:54 +0900

    [level 2] Title: 주식가격, Time: 22.95 ms, Memory: 60.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,10 +1,15 @@
 function solution(prices) {
-  const result = [];
+  let result = [];
+
   for (let i = 0; i < prices.length; i++) {
+    // 각 시점마다 유지시간을 새로 계산한다.
     let time = 0;
+
     for (let j = i + 1; j < prices.length; j++) {
+      // 다음 시점으로 이동했으므로 하락 여부와 상관없이 1초 증가
       time++;
 
+      // 현재 가격보다 낮아진 최초 시점에서 반복문 종료
       if (prices[i] > prices[j]) break;
     }
     result.push(time);
