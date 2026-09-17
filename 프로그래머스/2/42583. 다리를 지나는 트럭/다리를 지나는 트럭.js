fe7e234384e9f8e8a218526a835788f0fa0c66a3
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2026-09-17 12:37:16 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 870.40 ms, Memory: 50.9 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,36 +1,39 @@
 function solution(bridge_length, weight, truck_weights) {
-  // 1. 다리 길이 만큼의 배열 생성해서 0으로 채움
-  const bridge = Array.from({ length: bridge_length }).fill(0);
+  // 다리위에 있는 트럭 배열 생성
+  // 누적합을 이용하여 "weight" 초과 여부를 결정하여 트럭이 다리위에 올라갈 수 있는지 판단 플래그
+  const truckOnBridge = Array.from({ length: bridge_length - 1 }).fill(0);
+  // 첫 번쨰 트럭은 무조건 다리위에 올라갈 수 있으므로 올려놈
+  const firstTruck = truck_weights.shift();
+  truckOnBridge.push(firstTruck);
+  // 첫 번째 트럭이 이미 올라가있으므로 시간은 1초부터 시작
+  let time = 1;
 
-  let currentBridgeWeight = 0; // 2.다리를 건널 수 있는 최대 무게
-  let time = 0; // 3. 시간
-
-  // 4. 트럭무게 배열로만 가지고는 판단할 수없다
-  // -> 마지막 트럭이 다리에 올라가는 순간 반복문 종료 되기 때문에
-  // 5. 둘 조건이 모두 참일 경우에 반복문 종료
-  while (truck_weights.length > 0 || currentBridgeWeight > 0) {
+  // 반복문 조건도 대기열 트럭이랑 다리위에 있는 트럭이 둘다 없어야지 종료
+  while (
+    truck_weights.length > 0 ||
+    truckOnBridge.reduce((acc, cur) => acc + cur, 0) > 0
+  ) {
+    //시간 증가
     time++;
-    // 6. 다리위에서 빠져나간 트럭
-    const exitedTruck = bridge.shift();
-    // 7. 다리위에서 빠져나갔으므로 현재 다리 무게 최신화
-    currentBridgeWeight -= exitedTruck;
-    // 8. 대기중인 트럭 첫번째 요소 꺼냄 <- 비교를 위해서 임시로 꺼냄
-    const truck = truck_weights[0];
-    // 9. 다리에 올라갈 수 있는지 판단
-    const isPossibleToPassBridge = truck + currentBridgeWeight <= weight;
 
-    if (isPossibleToPassBridge) {
-      // 10. 트럭대기열에서 첫번째요소 꺼냄
-      const truckWeight = truck_weights.shift();
-      // 11. 다리위에 올라감
-      bridge.push(truckWeight);
-      // 12. 현재 다리무게에 더함
-      currentBridgeWeight += truckWeight;
+    // 1초가 지났으므로 맨 앞 트럭 이동
+    truckOnBridge.shift();
+
+    // 다리 현재무게
+    const currentWeight = truckOnBridge.reduce((acc, cur) => acc + cur, 0);
+
+    // 트럭위에 올라갈 수 플래그
+    const isPossibleToOnBridge =
+      truck_weights.length > 0 && currentWeight + truck_weights[0] <= weight;
+
+    if (isPossibleToOnBridge) {
+      // 가능하다면 다음 트럭 shift() -> push()
+      const nextTruck = truck_weights.shift();
+      truckOnBridge.push(nextTruck);
     } else {
-      // 13. 올라갈 수 없다면 다리에 0 푸시 <- 이것도 시간에 포함하기 위해서 포함을 시켜야댐
-      bridge.push(0);
+      // 불가능하다면 0을 Push()
+      truckOnBridge.push(0);
     }
   }
-
   return time;
 }
