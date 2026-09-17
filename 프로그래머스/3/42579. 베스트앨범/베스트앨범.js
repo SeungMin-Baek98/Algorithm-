fe36ca3d964e8b27ce8fc6af3a3cc231ed96d602
백승민 <158240718+SeungMin-Baek98@users.noreply.github.com>
2026-09-17 12:07:54 +0900

    [level 3] Title: 베스트앨범, Time: 0.30 ms, Memory: 44.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -1,37 +1,34 @@
 function solution(genres, plays) {
-  const songInfoObj = {};
-  const result = [];
+  const obj = {};
+
   for (let i = 0; i < genres.length; i++) {
     const genre = genres[i];
     const play = plays[i];
 
-    if (!songInfoObj[genre]) {
-      songInfoObj[genre] = {
+    if (!obj[genre]) {
+      obj[genre] = {
         totalPlay: 0,
-        songs: {},
+        songs: [],
       };
     }
 
-    songInfoObj[genre].totalPlay += play;
-    songInfoObj[genre].songs[i] = play;
-  }
-  const sortedByTotalPlay = Object.entries(songInfoObj).sort(
-    ([genreA, dataA], [genreB, dataB]) => dataB.totalPlay - dataA.totalPlay,
-  );
-
-  for (const [genre, data] of sortedByTotalPlay) {
-    const sortedBySongPlay = Object.entries(data.songs).sort(
-      ([songIdxA, songPlayA], [songIdxB, songPlayB]) => {
-        if (songPlayA === songPlayB) return songIdxA - songIdxB;
-
-        return songPlayB - songPlayA;
-      },
-    );
-
-    for (const [idx, play] of sortedBySongPlay.slice(0, 2)) {
-      result.push(idx);
-    }
+    obj[genre].totalPlay += play;
+    obj[genre].songs.push({
+      idx: i,
+      play,
+    });
   }
 
-  return result.map((item) => +item);
+  const result = [];
+
+  const sortPlay = Object.values(obj).sort((a, b) => b.totalPlay - a.totalPlay);
+
+  for (const { songs } of sortPlay) {
+    songs
+      .sort((a, b) => b.play - a.play || a.idx - b.idx)
+      .slice(0, 2)
+      .forEach(({ idx }) => result.push(idx));
+  }
+
+  return result;
 }
